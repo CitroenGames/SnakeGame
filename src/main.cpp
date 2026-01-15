c3b0b5e3a37f5082b73d3630a6ce6d6939abc981
CitroenGames <54474809+CitroenGames@users.noreply.github.com>
2026-01-15 10:30:44 +0100

    simple sdl3 window

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,54 @@
 #include <iostream>
+#include "SDL3/SDL.h"
 
+int main(int argc, char* argv[]) {
+    // Unused argc, argv
+    (void)argc;
+    (void)argv;
 
-int main() {
-	std::cout << "Hello, World!" << std::endl;
-	return 0;
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+        std::cerr << "SDL could not initialize! SDL_Error: " << SDL_GetError() << std::endl;
+        return 1;
+    }
+
+    SDL_Window* window = SDL_CreateWindow(
+        "SnakeGame SDL3 Window",
+        800, 600,
+        SDL_WINDOW_RESIZABLE
+    );
+
+    if (window == nullptr) {
+        std::cerr << "Window could not be created! SDL_Error: " << SDL_GetError() << std::endl;
+        SDL_Quit();
+        return 1;
+    }
+
+    SDL_Renderer* renderer = SDL_CreateRenderer(window, nullptr);
+    if (renderer == nullptr) {
+        std::cerr << "Renderer could not be created! SDL_Error: " << SDL_GetError() << std::endl;
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+        return 1;
+    }
+
+    bool quit = false;
+    SDL_Event e;
+
+    while (!quit) {
+        while (SDL_PollEvent(&e) != 0) {
+            if (e.type == SDL_EVENT_QUIT) {
+                quit = true;
+            }
+        }
+
+        SDL_SetRenderDrawColor(renderer, 0xFF, 0xFF, 0xFF, 0xFF);
+        SDL_RenderClear(renderer);
+        SDL_RenderPresent(renderer);
+    }
+
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+
+    return 0;
 }
